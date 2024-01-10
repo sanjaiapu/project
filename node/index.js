95343798a46c560704eda2b390cbf16ff1c07f68
snjai <153905376+sanjaiapu@users.noreply.github.com>
2024-01-10 15:34:34 +0530

    my comitments

--- node/index.js
+++ node/index.js
@@ -4,14 +4,13 @@ const express =require("express");
 const port =5000;
 
 const app =express();
-
+add=.env file
+PORTS=5000
 const dotenv =require("dotenv").config();
+const PORTS =5000
 
-const ports =process.env.port || 5000;
+const PORTS =process.env.port || 5000;
 
 app.get("/api/home",(req,res)=>{
     res.send("weclome API ")
-});
-app.get("/api/",(req,res)=>{
-    res.send("weclome API about")
 });
